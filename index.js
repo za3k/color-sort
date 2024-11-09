957e7c479c1552fad8abd3c56b2a0a3766ce6869
Zachary Vance <za3k@za3k.com>
2024-11-08 20:46:34 -0500

    Final version

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ function shuffle(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
+function emptyArray(n, e) {
+    var a = []
+    for (var i=0; i<n; i++) a.push(e)
+    return a
+}
+function invertDict(d) {
+    var d2 = {}
+    for (var k in d) d2[d[k]] = k
+    return d2
+}
+function sortKeysByValue(d) {
+    ks = Object.keys(d)
+    ks.sort((a, b) => { return d[b] - d[a] })
+    console.log(d, ks)
+    return ks
+}
+
 
 // ----- Utility functions (color) -----
 function okayColor(color) {
@@ -74,7 +91,6 @@ function cssColor(color) {
 }
 
 // ----- Level generation -----
-const NUMBER = 48
 const NUM_GROUPS = 4
 const PIECE_SIZE = 50 // In pixels
 
@@ -82,8 +98,8 @@ function randomPlace() {
     const width = $(".game").width()
     const height = $(".game").height()
 
-    const x = randFloat(0.1*width, 0.9*width - PIECE_SIZE)
-    const y = randFloat(0.1*height, 0.9*height - PIECE_SIZE)
+    const x = randFloat(0.01*width, 0.99*width - PIECE_SIZE)
+    const y = randFloat(0.01*height, 0.99*height - PIECE_SIZE)
     return {x, y}
 }
 
@@ -93,7 +109,7 @@ function randomPlaces(number) {
     return l
 }
 
-function randomBaseColors(n, minDistance) {
+function randomBaseColors(n, minDistance, maxDistance) {
     var colors = []
     for (var i=0; i<n; i++) {
         var failed = true
@@ -102,6 +118,7 @@ function randomBaseColors(n, minDistance) {
             failed = false
             for (var j=0; j<i; j++) {
                 if (colorDistance(colors[j], colors[i]) < minDistance) failed = true
+                if (colorDistance(colors[j], colors[i]) > maxDistance) failed = true
             }
         }
     }
@@ -109,18 +126,19 @@ function randomBaseColors(n, minDistance) {
 }
 
 function difficultyToColorDistance(difficulty) {
-    return Math.pow(2, 1/difficulty) * 10
+    // First two numbers are how different the groups are (3-30)
+    // Second number is how much variation within a group
+    return [30, 1000, 9]
 }
 function randomColors(difficulty, number) {
     var l = []
-    const colorDistance = difficultyToColorDistance(difficulty)
-    const baseColors = randomBaseColors(NUM_GROUPS, colorDistance)
-    console.log(baseColors)
+    const [bigMin, bigMax, small] = difficultyToColorDistance(difficulty)
+    const baseColors = randomBaseColors(NUM_GROUPS, bigMin, bigMax)
     // Generate
     for (var g=0; g<NUM_GROUPS; g++) {
         for (var i=0; i<number/NUM_GROUPS; i++) {
             l.push({
-                color: randomNearbyColor(baseColors[g], colorDistance/3),
+                color: randomNearbyColor(baseColors[g], small),
                 group: g
             })
         }
@@ -143,12 +161,42 @@ function generatePiece(data, place) {
 
 function generateLevel(difficulty, number) {
     if (number % NUM_GROUPS != 0) number += NUM_GROUPS-(number % NUM_GROUPS)
-    console.log(difficulty, number)
 
     const colors = randomColors(difficulty, number)
     const places = randomPlaces(number)
 
     for (var i=0; i<number; i++) generatePiece(colors[i], places[i])
+}
+
+// ----- Win logic -----
+// piece.quadrant, piece.group
+function bestQuadsForGroup(g, pieces) {
+    const quadScore = {0:0, 1:0, 2:0, 3:0}
+    for (var piece of pieces) {
+        if (piece.quadrant < 0) continue
+        if (piece.group == g) {
+            quadScore[piece.quadrant] += 1
+        } else {
+            quadScore[piece.quadrant] -= 0.5
+        }
+    }
+    return sortKeysByValue(quadScore)
+}
+function assignQuadrants(pieces) {
+    const quadClaimed = [false, false, false, false]
+    const ret = {}
+    for (var g=0; g < 4; g++) {
+        const bq = bestQuadsForGroup(g, pieces)
+        console.log({g, bq, quadClaimed, ret, pieces})
+        for (var q of bestQuadsForGroup(g, pieces)) {
+            if (!quadClaimed[q]) {
+                quadClaimed[q] = true
+                ret[q] = g
+                break
+            }
+        }
+    }
+    return ret
 }
 
 // ----- Drag and drop -----
@@ -165,13 +213,13 @@ function movePiece(piece, place) {
 }
 
 function startDrag(e) {
-    console.log("mousedown(startDrag)", e)
+    //console.log("mousedown(startDrag)", e)
     if (e.button > 0) return;
 
     const piece = $(e.target)
     piece.addClass("dragged")
 
-    const offset = { x: e.offsetX, y: e.offsetY }
+    const offset = { x: e.offsetX + 3, y: e.offsetY + 3 } // 3 is the border width
     e.preventDefault()
 
     function dragLocation(e) {
@@ -184,12 +232,13 @@ function startDrag(e) {
     $(window).on("mousemove", (e) => {
         place = dragLocation(e)
         // Show drag
-        console.log("mousemove", e, place)
+        //console.log("mousemove", e, place)
         movePiece(piece, place)
     })
 
     $(window).on("mouseup", (e) => {
-        console.log("mouseup", e)
+        //console.log("mouseup", e)
+        place = dragLocation(e)
 
         piece.removeClass("dragged")
         $(window).off("mouseup")
@@ -202,10 +251,13 @@ function startDrag(e) {
 }
 
 // -----  UI  -----
+const ANIMATION_TIME = 2000 // Time to show pieces moving, in ms
+
 var everStarted = false
 function startGame() {
     $(".between-games").hide()
     $(".in-game").show()
+    $(".bit").remove()
 
     if (!everStarted) {
         everStarted = true
@@ -213,27 +265,80 @@ function startGame() {
     }
 
     const difficulty = Number($("#difficulty").val())
-    generateLevel(difficulty, NUMBER)
+    const number = getNumber()
+    generateLevel(difficulty, number)
 }
 
 function gameOver() {
-    $(".between-games").hide()
-    $(".in-game").show()
+    $(".between-games").show()
+    $(".in-game").hide()
+}
+
+function animateMove(piece, quad, durationMs) {
+    const target = {
+        x: quad.position().left + randFloat(0, quad.width()-PIECE_SIZE),
+        y: quad.position().top + randFloat(0, quad.height()-PIECE_SIZE)
+    }
+
+    piece.animate({
+        top: target.y,
+        left: target.x
+    }, durationMs)
 }
 
 function donePressed() {
+    const pieces = []
+    const quadrants = [$(".quadrant.top.left"), $(".quadrant.top.right"), $(".quadrant.bottom.left"), $(".quadrant.bottom.right")]
+    const width = $(".game").width()
+    const height = $(".game").height()
 
+    for (var piece of $(".bit")) {
+        // Note its quadrant
+        piece = $(piece)
+        const place = piece.position()
+        var quadrant = 0 + (place.left > width/2) + 2*(place.top > height/2)
+        // Pieces in the + empty space
+        if ((0.4*width < place.left && place.left < 0.6*width-PIECE_SIZE) ||
+            (0.4*height < place.top && place.top < 0.6*height-PIECE_SIZE)) quadrant = -1;
+
+        piece.data("quadrant", quadrant)
+        pieces.push({
+            quadrant: piece.data("quadrant"),
+            group: piece.data("group"),
+            e: piece
+        })
+    }
+    const quad2group = assignQuadrants(pieces)
+    quad2group[-1] = -1
+    const group2quad = invertDict(quad2group)
+
+    for (piece of pieces) {
+        if (piece.group != quad2group[piece.quadrant]) {
+            animateMove(piece.e, quadrants[group2quad[piece.group]], ANIMATION_TIME)
+        }
+    }
+
+    setTimeout(gameOver, ANIMATION_TIME)
 }
 
+function getNumber() {
+    const value = $("#number")[0].value
+    const number = $(".number-section datalist").find(`option[value=${value}]`).data("number")
+    return Number(number)
+}
+
+function updateNumber() { $("label[for=number]").text(getNumber()) }
 $(document).ready(() => {
-    $(".in-game").hide()
+    $(".in-game:not(.title)").hide()
     $(".button.done").on("click", donePressed)
     $(".button.start").on("click", startGame)
+    $("#number").on("input", updateNumber)
 })
 
 function firstStart() {
     const aud = document.getElementById("audio")
     aud.volume = 0.2
     aud.play()
+    $(".pulse").removeClass("pulse")
 }
 
